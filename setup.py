27f1c6f9be89a497c193a27418bb2206dfc5963e
abenezer <hi@abenezer.sh>
2023-05-10 20:38:41 -0700

    change dir structure to make it buildable

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ Flask-RL
 
 flask rate limiter
 """
+from os import path
 import versioneer
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
@@ -19,13 +20,14 @@ setup(
     name='flask_rl',
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    url='http://github.com/a6enez3r/flask_rl',
+    url='http://git.192.168.1.182.nip.io:3000/a6enez3r/flask_rl/',
     author="Abenezer Mamo",
     author_email="hi@abenezer.sh",
     license="MIT",
     description='flask rate limiter',
     long_description=long_description,
-    py_modules=['flask_rl'],
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     zip_safe=False,
     include_package_data=True,
     platforms='any',
